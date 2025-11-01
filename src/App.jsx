fc8bc830ece6855e9ae30e514b972ff31d96f4e1
karthikeyan sivasankar <karthikaian81.kk@gmail.com>
2025-11-02 05:28:49 +0530

    latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <h2>KARTHIK CI CD PIPELINE LEARNINGS</h2>
-      <h6>Last Changes Deployed on ${new Date().toLocaleString()}</h6>
+      <h6>Last Changes Deployed on {new Date().toLocaleString()}</h6>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
